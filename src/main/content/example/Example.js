ea6e5990bac29c2cf23770a85cfcd0e417cb66a1
Paul B. Hartzog <PaulBHartzog@PaulBHartzog.org>
2018-09-10 14:21:14 -0400

    added table

--- src/main/content/example/Example.js
+++ src/main/content/example/Example.js
@@ -2,36 +2,77 @@ import React, {Component} from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import {FusePageSimple, DemoContent} from '@fuse';
 
+import PropTypes from 'prop-types';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
 const styles = theme => ({
-    layoutRoot: {}
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
 });
 
-class Example extends Component {
-
-    render()
-    {
-        const {classes} = this.props;
-        return (
-            <FusePageSimple
-                classes={{
-                    root: classes.layoutRoot
-                }}
-                header={
-                    <div className="p-24"><h4>Header</h4></div>
-                }
-                contentToolbar={
-                    <div className="px-24"><h4>Content Toolbar</h4></div>
-                }
-                content={
-                    <div className="p-24">
-                        <h4>Content</h4>
-                        <br/>
-                        <DemoContent/>
-                    </div>
-                }
-            />
-        )
-    }
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
 }
 
-export default withStyles(styles, {withTheme: true})(Example);
+const rows = [
+  createData('Frozen yogurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function SimpleTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell numeric>Calories</TableCell>
+            <TableCell numeric>Fat (g)</TableCell>
+            <TableCell numeric>Carbs (g)</TableCell>
+            <TableCell numeric>Protein (g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => {
+            return (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell numeric>{row.calories}</TableCell>
+                <TableCell numeric>{row.fat}</TableCell>
+                <TableCell numeric>{row.carbs}</TableCell>
+                <TableCell numeric>{row.protein}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTable);
